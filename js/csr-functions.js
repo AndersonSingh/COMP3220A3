2bef445767dbbfc95a30ca53e1e74f4c3e8bdbb4
Anderson Singh <andersonsingh@outlook.com>
2016-04-01 21:48:26 -0400

    edited the csr dashboard and default message for task

---
+++ js/csr-functions.js
@@ -0,0 +1,12 @@
+$(document).on('click', '#default-message-submit-button', function(e){
+    
+    window.location="/csr_success_reponse.html";
+    return false;
+})
+
+
+$(document).on('click', '#sign-out-button', function(e){
+    
+    window.location = "/index.html";
+    return false;
+})
